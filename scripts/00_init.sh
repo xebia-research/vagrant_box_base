d406966e0fe8681705fa73ff3af35dd0e990361d
Jeroen Willemsen <jwillemsen@xebia.com>
2018-02-15 13:29:30 +0100

    First cleanup

--- scripts/00_init.sh
+++ scripts/00_init.sh
@@ -11,13 +11,13 @@ sudo apt-get install -y git
 echo "Installing required packages..."
 sudo apt-get install -y -q python-software-properties build-essential autotools-dev automake pkg-config ant zsh expect
 
-echo "Configuring ZSH..."
-sudo apt-get install -y zsh
-if [ ! -d ~vagrant/.oh-my-zsh ]; then
-  git clone https://github.com/robbyrussell/oh-my-zsh.git ~vagrant/.oh-my-zsh
-fi
-
-chsh -s /bin/zsh vagrant
+# echo "Configuring ZSH..."
+# sudo apt-get install -y zsh
+# if [ ! -d ~vagrant/.oh-my-zsh ]; then
+#   git clone https://github.com/robbyrussell/oh-my-zsh.git ~vagrant/.oh-my-zsh
+# fi
+#
+# chsh -s /bin/zsh vagrant
 
 echo "Updating apt-get repos..."
 
@@ -26,4 +26,3 @@ sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /et
 
 # This will also call apt-get update
 curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
-
